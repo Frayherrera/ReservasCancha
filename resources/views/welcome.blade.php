1d89e533ff16f192068922e4e966e901d6263873
frayherrera <ufonefray@gmail.com>
2026-08-18 22:35:03 -0500

    titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
-
+<title>ReservaFutbol - Reserva tu cancha</title>
 @push('styles')
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>ReservaFutbol - Reserva tu cancha</title>
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="preconnect" href="https://fonts.googleapis.com">
